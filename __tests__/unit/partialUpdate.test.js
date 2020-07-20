d2ef649d708c3c24c7fffa0a300310d870c456d5
Dru Serkes <DruSerkes@gmail.com>
2020-07-20 18:42:55 -0400

    Unittest: partialUpdate

--- __tests__/unit/partialUpdate.test.js
+++ __tests__/unit/partialUpdate.test.js
@@ -1,9 +1,20 @@
-describe("partialUpdate()", () => {
-  it("should generate a proper partial update query with just 1 field",
-      function () {
+process.env.NODE_ENV = 'test';
+const partialUpdate = require('../../helpers/partialUpdate');
 
-    // FIXME: write real tests!
-    expect(false).toEqual(true);
-
-  });
+describe('partialUpdate()', () => {
+	it('should generate a proper partial update query with just 1 field', function() {
+		// FIXME: write real tests!
+		const result = partialUpdate(
+			'users',
+			{ happy: true, height: 72, home: 'where the heart is' },
+			'username',
+			'test'
+		);
+		expect(result).toBeInstanceOf(Object);
+		expect(result.query).toBeTruthy();
+		expect(result.query).toEqual(`UPDATE users SET happy=$1, height=$2, home=$3 WHERE username=$4 RETURNING *`);
+		expect(result.values).toBeTruthy();
+		expect(result.values).toBeInstanceOf(Array);
+		expect(result.values).toEqual([ true, 72, 'where the heart is', 'test' ]);
+	});
 });
