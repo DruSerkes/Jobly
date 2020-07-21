e416e498400012cc7665005a35c89d146db0a6a5
Dru Serkes <DruSerkes@gmail.com>
2020-07-21 18:54:17 -0400

    define and create users table

--- data.sql
+++ data.sql
@@ -1,7 +1,8 @@
 \c jobly
 
-DROP TABLE companies;
 DROP TABLE jobs;
+DROP TABLE companies;
+DROP TABLE users;
 
 
 CREATE TABLE companies
@@ -21,5 +22,32 @@ CREATE TABLE jobs
     salary FLOAT NOT NULL,
     equity FLOAT NOT NULL CHECK (equity < 1),
     company_handle TEXT REFERENCES companies ON DELETE CASCADE,
-    date_posted TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-)
+    date_posted TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (company_handle) REFERENCES companies (handle)
+);
+
+CREATE TABLE users
+(
+    username TEXT PRIMARY KEY,
+    password TEXT NOT NULL,
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    email TEXT NOT NULL UNIQUE,
+    photo_url TEXT,
+    is_admin BOOLEAN NOT NULL DEFAULT FALSE
+);
+
+-- INSERT INTO companies
+--     (handle, name)
+-- VALUES
+--     ('lhfh', 'LHFH Records');
+
+-- INSERT INTO jobs
+--     (title, salary, equity, company_handle)
+-- VALUES
+--     ('producer', 50.00, 0.1, 'lhfh');
+
+-- INSERT INTO users
+--     (username, password, first_name, last_name, email)
+-- VALUES
+--     ('proddy', 'password', 'prod', 'ucer', 'proddy@prod.com');
