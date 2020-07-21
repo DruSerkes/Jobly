50287053361a0f7381f4356620b07f01df715b55
Dru Serkes <DruSerkes@gmail.com>
2020-07-21 00:36:50 -0400

    add helmet

--- app.js
+++ app.js
@@ -1,37 +1,42 @@
 /** Express app for jobly. */
 
-const express = require("express");
+const express = require('express');
 
-const ExpressError = require("./helpers/expressError");
+const ExpressError = require('./helpers/expressError');
 
-const morgan = require("morgan");
+const morgan = require('morgan');
+
+const helmet = require('helmet');
 
 const app = express();
 
 app.use(express.json());
 
+// add helmet
+app.use(helmet());
+
 // add logging system
-app.use(morgan("tiny"));
+app.use(morgan('tiny'));
 
 /** 404 handler */
 
 app.use(function(req, res, next) {
-  const err = new ExpressError("Not Found", 404);
+	const err = new ExpressError('Not Found', 404);
 
-  // pass the error to the next piece of middleware
-  return next(err);
+	// pass the error to the next piece of middleware
+	return next(err);
 });
 
 /** general error handler */
 
 app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  console.error(err.stack);
+	res.status(err.status || 500);
+	console.error(err.stack);
 
-  return res.json({
-    status: err.status,
-    message: err.message
-  });
+	return res.json({
+		status  : err.status,
+		message : err.message
+	});
 });
 
 module.exports = app;
