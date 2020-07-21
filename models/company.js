c89a63db77b9d80ae00b5b34b4f05711f4a7f427
Dru Serkes <DruSerkes@gmail.com>
2020-07-21 14:52:34 -0400

    Company model: add method -  update

--- models/company.js
+++ models/company.js
@@ -2,6 +2,7 @@ const db = require('../db');
 const ExpressError = require('../helpers/expressError');
 const getSqlParameters = require('../helpers/getSqlParameters');
 const partialUpdate = require('../helpers/partialUpdate');
+const sqlForPartialUpdate = require('../helpers/partialUpdate');
 
 class Company {
 	/** get all companies 
@@ -27,7 +28,7 @@ class Company {
      * @returns a single company from db with matching handle 
      */
 	static async getByHandle(handle) {
-		const result = await db.query(`SELECT * FROM companies WHERE handle ILIKE $1`, [ handle ]);
+		const result = await db.query(`SELECT * FROM companies WHERE handle ILIKE $1`, [ handle.toLowerCase() ]);
 
 		if (!result.rows.length) {
 			throw new ExpressError(`No company found with handle: ${handle}`, 404);
@@ -48,11 +49,16 @@ class Company {
 		const result = await db.query(
 			`INSERT INTO companies (handle, name)
             VALUES ($1, $2) RETURNING handle, name`,
-			[ handle, name ]
+			[ handle.toLowerCase(), name ]
 		);
 		return result.rows[0];
 	}
 
+	/** Remove a company 
+     * 
+     * @param {*} handle a company handle to identify which company to remove
+     * @returns nothing
+     */
 	static async remove(handle) {
 		const result = await db.query(`DELETE FROM companies WHERE handle=$1 RETURNING handle`, [ handle ]);
 
@@ -61,14 +67,16 @@ class Company {
 		}
 	}
 
-	static async update(id, newName, newAge) {
-		const result = await db.query(
-			`UPDATE cats SET name = $1, age = $2
-    WHERE id = $3
-    RETURNING id, name, age`,
-			[ newName, newAge, id ]
-		);
-		if (!result.rows.length) throw new ExpressError('Cat not found', 404);
+	/** Update a company 
+     * 
+     * @param {*} items object (req.body) with optional params: name (str), num_employees (int), description (text), logo_url (text)  
+     * @param {*} handle handle for company to update 
+     * @returns updated company 
+     */
+	static async update(items, handle) {
+		const { query, values } = sqlForPartialUpdate('companies', items, 'handle', handle);
+		const result = await db.query(query, values);
+		if (!result.rows.length) throw new ExpressError(`Company not found with handle ${handle}`, 404);
 		return result.rows[0];
 	}
 }
