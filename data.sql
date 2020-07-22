c171160c80cb8a53cde098e6c069fadbe7e37841
Dru Serkes <DruSerkes@gmail.com>
2020-07-22 09:05:17 -0400

    add table for applications

--- data.sql
+++ data.sql
@@ -3,6 +3,7 @@
 DROP TABLE jobs;
 DROP TABLE companies;
 DROP TABLE users;
+DROP TABLE applications;
 
 
 CREATE TABLE companies
@@ -21,7 +22,7 @@ CREATE TABLE jobs
     title TEXT NOT NULL,
     salary FLOAT NOT NULL,
     equity FLOAT NOT NULL CHECK (equity < 1),
-    company_handle TEXT REFERENCES companies ON DELETE CASCADE,
+    company_handle TEXT REFERENCES companies(handle) ON DELETE CASCADE,
     date_posted TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (company_handle) REFERENCES companies (handle)
 );
@@ -37,6 +38,19 @@ CREATE TABLE users
     is_admin BOOLEAN NOT NULL DEFAULT FALSE
 );
 
+
+CREATE TABLE applications
+(
+    username TEXT REFERENCES users(username) ON DELETE CASCADE,
+    job_id INT REFERENCES jobs(id) ON DELETE CASCADE,
+    state TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY(username, job_id)
+
+);
+
+
+
 INSERT INTO companies
     (handle, name)
 VALUES
@@ -51,3 +65,8 @@ INSERT INTO users
     (username, password, first_name, last_name, email)
 VALUES
     ('proddy', 'password', 'prod', 'ucer', 'proddy@prod.com');
+
+INSERT INTO applications
+    (username, job_id, state)
+VALUES
+    ('proddy', 1, 'NY')
