c40cc476450218d662fecfba758d598ee339c829
Dru Serkes <DruSerkes@gmail.com>
2020-07-21 23:14:06 -0400

    add bcrypt work factor to env/config

--- config.js
+++ config.js
@@ -3,8 +3,8 @@
 require('dotenv').config();
 
 const SECRET_KEY = process.env.SECRET_KEY || 'test';
-
 const PORT = +process.env.PORT || 3000;
+const BCRYPT_WORK_FACTOR = +process.env.BCRYPT_WORK_FACTOR || 12;
 
 // database is:
 //
@@ -23,5 +23,6 @@ if (process.env.NODE_ENV === 'test') {
 module.exports = {
 	SECRET_KEY,
 	PORT,
-	DB_URI
+	DB_URI,
+	BCRYPT_WORK_FACTOR
 };
